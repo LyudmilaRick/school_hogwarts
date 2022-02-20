ae4800b42edc1072a9f06a9bdb5fd6a3f29019ae
RickLyudmila <Lrick@yandex.ru>
2022-02-20 21:12:26 +0300

    Lesson Data Base.First commit

---
+++ src/main/java/ru/hogwarts/rick/school_hogwarts/SchoolHogwartsApplication.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.rick.school_hogwarts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolHogwartsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolHogwartsApplication.class, args);
+    }
+
+}
